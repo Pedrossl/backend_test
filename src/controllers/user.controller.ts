2313594c03b64c222bdaf3cf0c1963d561ce4c45
Pedro Lobato <pedrosslobato@gmail.com>
2023-08-26 12:35:20 -0300

    :sparkles: Added Added password encryption

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,15 +3,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserModel } from 'src/models/user.model';
 import { UserSchema } from 'src/schemas/user.schema';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Controller('user')
 export class UserController {
   constructor(
     @InjectRepository(UserModel) private model: Repository<UserModel>,
   ) {}
+
   @Post()
-  async create(@Body() body: UserSchema) {
-    return body;
+  async create(@Body() body: UserSchema): Promise<{ data: UserModel }> {
+    const saltRounds = 15;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(body.password, salt);
+
+    const userToCreate: UserModel = {
+      ...body,
+      password: hashedPassword,
+      id: 0,
+    };
+
+    const user = await this.model.save(userToCreate);
+    return { data: user };
   }
 
   @Get()
