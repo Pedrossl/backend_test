2bbb61cc0ead7e2ec6ab6050e329f599c510ae77
Pedro Lobato <pedrosslobato@gmail.com>
2023-08-28 18:36:04 -0300

    :wrench: Added userServivece providers and exports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserModel } from './user.model';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
+import { UserService } from './user.sevice';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserModel])],
   controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
