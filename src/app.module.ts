0733f796931b2b733b9c86cdeea4ab575f97855b
Pedro Lobato <pedrosslobato@gmail.com>
2023-08-28 17:04:16 -0300

    :truck: changing userMolule directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './modules/user.module';
+import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
