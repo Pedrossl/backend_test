a79d5175f492252cca81310873a03cfcc3ceb652
Pedro Lobato <pedrosslobato@gmail.com>
2023-08-28 18:32:39 -0300

    :closed_lock_with_key: Added user validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.sevice';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string): any {
-    throw new Error('Method not implemented.');
+  constructor(private readonly userController: UserService) {}
+  async validateUser(email: string, password: string) {
+    const user = await this.userController.findByEmail(email);
+    console.log(user);
+
+    if (user) {
+      console.log('Dale');
+      console.log(user.password);
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+      if (isPasswordValid) {
+        console.log('Validou');
+
+        return { ...user, password: undefined };
+      }
+    }
+    throw new Error('Invalid email or password');
   }
 }
