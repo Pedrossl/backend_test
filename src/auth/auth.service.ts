2b420b3794a06d3de70194cf3c385bf4ab787411
Pedro Lobato <pedrosslobato@gmail.com>
2023-08-28 21:07:19 -0300

    :lock:creating userpayload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.sevice';
 import * as bcrypt from 'bcrypt';
+import { UserModel } from 'src/user/user.model';
+import { UserPayload } from 'src/user/userPayload';
+import { UserToken } from 'src/user/UserToken';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userController: UserService) {}
+  constructor(
+    private readonly userController: UserService,
+    private JwtService: JwtService,
+  ) {}
+  login(user: UserModel): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+    const token = this.JwtService.sign(payload);
+    return {
+      accessToken: token,
+    };
+  }
+
   async validateUser(email: string, password: string) {
     const user = await this.userController.findByEmail(email);
     console.log(user);
