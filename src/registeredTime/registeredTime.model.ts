95e8722d64638f4c12e67d35765ee61050f893b7
Pedro Lobato <pedrosslobato@gmail.com>
2023-08-29 11:51:27 -0300

    :card_file_box: created new model

---
+++ src/registeredTime/registeredTime.model.ts
@@ -0,0 +1,21 @@
+import { UserModel } from 'src/user/user.model';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'registered_time' })
+export class RegisteredTimeModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  time_registered: Date;
+
+  @ManyToOne(() => UserModel)
+  @JoinColumn({ name: 'user_id' })
+  user: number;
+}
